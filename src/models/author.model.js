f7565f9d267841cc35b741e53919cea39712278a
Lara Martin <lara.martin.lagares@gmail.com>
2024-06-02 11:00:22 +0200

    Corrección en controladores author y  post

--- src/models/author.model.js
+++ src/models/author.model.js
@@ -2,23 +2,23 @@
 
 const selectAllAuthors = () => {
     return db.query('SELECT * FROM Author');
-}
+};
 
 const selectAuthorById = (id_author) => {
     return db.query('SELECT * FROM Author WHERE idAuthor = ?', [id_author]);
-}
+};
 
 const insertAuthor = ({name, email, image}) => {
     return db.query('INSERT INTO Author (name, email, image) VALUES (?, ?, ?)', [name, email, image]);
-}
+};
 
 const updateAuthorById = (id_author, {name, email, image}) => {
     return db.query('UPDATE Author SET name = ?, email = ?, image = ? WHERE idAuthor = ?', [name, email, image, id_author]);
-}
+};
 
 const deleteAuthorById = (id_author) => {
     return db.query('DELETE FROM Author WHERE idAuthor = ?', [id_author]);
-}
+};
 
 
 module.exports = {
--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,28 +1,28 @@
-// Funciones con las queries que lanzamos contra la tabla Author
+// Funciones con las queries que lanzamos contra la tabla Post
 
 const selectAllPosts = () => {
     return db.query('SELECT post.idPost, author.idAuthor, author.name, author.email, author.image, post.title, post.description, post.date, post.category FROM post JOIN author ON post.idAuthor = author.idAuthor');
-}
+};
 
 const selectPostById = (id_post) => {
     return db.query('SELECT post.idPost, author.idAuthor, author.name, author.email, author.image, post.title, post.description, post.date, post.category FROM post JOIN author ON post.idAuthor = author.idAuthor WHERE post.idPost = ?', [id_post]);
-}
+};
 
 const selectPostsByAuthor = (id_author) => {
     return db.query('SELECT post.idPost, post.title, post.description, post.date, post.category FROM post WHERE post.idAuthor = ?', [id_author]);
-}
+};
 
 const insertPost = ({idAuthor, title, description, date, category}) => {
     return db.query('INSERT INTO Post (idAuthor, title, description, date, category) VALUES (?, ?, ?, ?, ?)', [idAuthor, title, description, date, category]);
-}
+};
 
 const updatePostById = (id_post, {idAuthor, title, description, date, category}) => {
     return db.query('UPDATE Post SET idAuthor = ?, title = ?, description = ?, date = ?, category = ? WHERE idPost = ?', [idAuthor, title, description, date, category, id_post]);
-}
+};
 
 const deletePostById = (id_post) => {
     return db.query('DELETE FROM Post WHERE idPost = ?', [id_post]);
-}
+};
 
 
 module.exports = {
