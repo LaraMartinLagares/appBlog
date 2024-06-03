296f2ece485cda2ddc285f29f7f83ec3e6f97f12
Lara Martin <lara.martin.lagares@gmail.com>
2024-06-03 09:37:51 +0200

    Implementación de middlewares para comprobación de ID de autor y de post

--- src/controllers/authors.controller.js
+++ src/controllers/authors.controller.js
@@ -2,27 +2,18 @@ const Author = require('../models/author.model');
 
 
 const getAllAuthors = async (req, res, next) => {
-
     try {
-        const [authors] = await Author.selectAllAuthors();  
+        const [ authors ] = await Author.selectAllAuthors();  
         res.json(authors);
     } catch (error) {
         next(error);
     }
 };
 
-
 const getAuthorById = async (req, res,  next) => {
-
-    const {id_author} = req.params;
-    
+    const { id_author } = req.params;
     try {
-        const [author] = await Author.selectAuthorById(id_author);
-
-        if (author.length === 0) {
-            return res.status(404).json({ error: 'Autor no encontrado'});
-        }
-
+        const [ author ] = await Author.selectAuthorById(id_author);
         res.json(author[0]);
 
     } catch (error) {
@@ -30,56 +21,46 @@ const getAuthorById = async (req, res,  next) => {
     }
 };
 
-
 const createAuthor = async (req, res, next) => {
-
     try {
-        const [result] = await Author.insertAuthor(req.body);
-        const [[author]] = await Author.selectAuthorById(result.insertId); 
+        const [ result ] = await Author.insertAuthor(req.body);
+        const [[ author ]] = await Author.selectAuthorById(result.insertId); 
         res.json(author);
-
     } catch (error) {
         next(error);
     } 
 };
 
 const updateAuthor = async (req, res, next) => {
-
     try {
-        const {id_author} = req.params;
-        const [result] = await Author.updateAuthorById(id_author, req.body);
-        
+        const { id_author } = req.params;
+        const [ result ] = await Author.updateAuthorById(id_author, req.body);
         if (result.changedRows === 1 ) {
-            const [[author]] = await Author.selectAuthorById(id_author); 
+            const [[ author ]] = await Author.selectAuthorById(id_author); 
             res.json(author);
         } else {
             res.status(400).json({ error: 'Se ha producido un error al actualizar el autor' });
-        }
-        
+        }    
     } catch (error) {
         next(error);
     } 
-
 };
 
-const deleteAuthor = async (req, res, next) => { //los errores de try/catch son propios de la aplicación (por lo que sea no responde- bbdd caída, fallo de conexión, la api no responde...)
-    
-    const {id_author} = req.params;
-
-    try{
-        
-        const [result] = await Author.deleteAuthorById(id_author);
-
+const deleteAuthor = async (req, res, next) => { 
+    const { id_author } = req.params;
+    try { 
+        const [ result ] = await Author.deleteAuthorById(id_author);
         if (result.affectedRows === 1) {
+            console.log(result);
             res.json({ message: 'Se ha borrado el autor y sus posts' });
         } else {
-            res.status(404).json({ message: 'El autor no existe' });
+            res.status(404).json({ error: 'Se ha producido un error al borrar el autor' });
         }
-
     } catch (error) {
         next(error);
     }
 };
+
 
 module.exports = {
     getAllAuthors, 
--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 
 const { getAllPosts, getPostById, getPostsByAuthor, createPost, updatePost, deletePost } = require('../../controllers/posts.controller');
+const { checkPostId, checkAuthorId } = require('../../helpers/middlewares');
 
 
 router.get('/', getAllPosts);
-router.get('/:id_post', getPostById);
-router.get('/author/:id_author', getPostsByAuthor);
+router.get('/:id_post', checkPostId, getPostById);
+router.get('/author/:id_author', checkAuthorId, getPostsByAuthor);
 router.post('/', createPost);
-router.put('/:id_post', updatePost); 
-router.delete('/:id_post', deletePost); 
+router.put('/:id_post', checkPostId, updatePost); 
+router.delete('/:id_post', checkPostId, deletePost); 
 
 
 module.exports = router;
